bd93e2fc431a90b1bc520e900b5de5d92973c8e4
Guilherme Soares Silvestre <gsoares.silvestre@gmail.com>
2023-11-17 02:42:05 -0300

    Added price vc mau, some safe checks on functions

--- tradespace.py
+++ tradespace.py
@@ -23,6 +23,30 @@ class TradespaceDesigner:
         return
 
     def add_performance_variable(self, name, min_value, max_value, units, weight):
+        # check if the name already exists in the performance items dictionary
+        if name in self.performance_items:
+            print(
+                f"Warning: {name} already exists in the performance items dictionary and will be overwritten"
+            )
+
+        # check if max value is greater than min value
+        if max_value < min_value:
+            raise Exception(
+                f"Warning: {name} has a max value that is less than the min value. Please change the values to calculate the SAU"
+            )
+
+        # check if the weight is between 0 and 1
+        if weight < 0 or weight > 1:
+            raise Exception(
+                f"Warning: {name} has a weight that is not between 0 and 1. Please change the values to calculate the MAU"
+            )
+
+        # check if the sum of the weights is higher than 1
+        if sum([item["weight"] for item in self.performance_items.values()]) + weight > 1:
+            raise Exception(
+                f"Warning: {name} has a weight that is too high. Please change the values to calculate the MAU"
+            )
+
         # add performance variable to the dict
         self.performance_items[name] = {
             "min_value": min_value,
@@ -184,17 +208,42 @@ class TradespaceDesigner:
 
         return
 
-    def calculate_sau(self):
+    def calculate_sau(self, clip_max=True, clip_min=True):
         # iterate over the performance variables
         for name, item in self.performance_items.items():
+
+            # check if the max value is greater than the max value in the tradespace
+            if self.tradespace_df[name].max() > item["max_value"] and not clip_max:
+                raise Exception(
+                    f"Warning: {name} has a max value that is less than the max value in the tradespace. Please change the values to calculate the SAU or set clip_max to True"
+                )
+            elif self.tradespace_df[name].max() > item["max_value"] and clip_max:
+                # set 1 to only where the max value is greater than the max value in the tradespace
+                self.tradespace_df.loc[self.tradespace_df[name] > item["max_value"], "SAU_" + name] = 1.0
+
+            # check if the min value is less than the min value in the tradespace
+            if self.tradespace_df[name].min() < item["min_value"] and not clip_min:
+                raise Exception(
+                    f"Warning: {name} has a min value that is greater than the min value in the tradespace. Please change the values to calculate the SAU or set clip_min to True"
+                )
+            elif self.tradespace_df[name].min() < item["min_value"] and clip_min:
+                # set 0 to only where the min value is less than the min value in the tradespace
+                self.tradespace_df.loc[self.tradespace_df[name] < item["min_value"], "SAU_" + name] = 0.0
+            
+            good_values = (self.tradespace_df[name] <= item["max_value"]) & (self.tradespace_df[name] >= item["min_value"])
             # calculate the SAU with a linear function
-            self.tradespace_df["SAU_" + name] = (
+            self.tradespace_df.loc[good_values, "SAU_" + name] = (
                 self.tradespace_df[name] - item["min_value"]
             ) / (item["max_value"] - item["min_value"])
 
-    def calculate_mau(self):
+    def calculate_mau(self, restrictive=True):
         self.tradespace_df["MAU"] = 0
         for name, item in self.performance_items.items():
+            
+            # check if the SAU is zero and zero out the MAU
+            if restrictive:
+                self.tradespace_df.loc[self.tradespace_df["SAU_" + name] == 0, "MAU"] = 0.0
+            
             self.tradespace_df["MAU"] += (
                 item["weight"] * self.tradespace_df["SAU_" + name]
             )
@@ -211,11 +260,16 @@ class TradespaceDesigner:
     def detect_pareto(self, x_name='price', y_name='MAU'):
 
         for index, row in self.tradespace_df.iterrows():
-            
+
             # get the x axis of the pareto search space
             x = row[x_name]
             y = row[y_name]
-            
+
+            # check if y is zero
+            if y == 0:
+                row['pareto'] = False
+                continue
+
             # check if the point is dominated by any other point
             if any((self.tradespace_df[x_name] < x) & (self.tradespace_df[y_name] > y)):
                 row['pareto'] = False
@@ -264,6 +318,38 @@ class TradespaceDesigner:
 
         plt.show(block=block)
 
+    def add_price_region(self, fig, desired_price_range):
+        if len(desired_price_range) != 2:
+            raise Exception(
+                "Warning: desired_price_range must have 2 values"
+            )
+
+        # check if the desired price is None
+        if desired_price_range[0] is None:
+            # set minimum price of all items to the minimum price
+            desired_price_range[0] = self.tradespace_df['price'].min()
+
+        if desired_price_range[1] is None:
+            # set maximum price of all items to the maximum price
+            desired_price_range[1] = self.tradespace_df['price'].max()
+
+        if desired_price_range[0] > desired_price_range[1]:
+            raise Exception(
+                "Warning: desired_price_range must have the first value less than the second value"
+            )
+
+        fig.add_vrect(
+            x0=desired_price_range[0],
+            x1=desired_price_range[1],
+            fillcolor="blue",
+            opacity=0.15,
+            line_width=0,
+            annotation_text=f"Desired Price Range",
+            annotation_position="top left",
+            annotation_font_size=14,
+        )
+        return fig
+
     def plot_tradespace_plotly(
         self,
         x_name,
@@ -274,6 +360,7 @@ class TradespaceDesigner:
         cbar_y=0.05,
         cbar_len= 0.9,
         pareto=True,
+        desired_price_range=[],
         showlegend=True,
     ):
         trace_list = []
@@ -300,7 +387,7 @@ class TradespaceDesigner:
                     colorbar=dict(len=cbar_len, y=cbar_y),
                 ),
                 text=[f"ID: {i}" for i in range(len(self.tradespace_df))],
-                hovertemplate="<b>%{text}</b><br><br>%{xaxis.title.text}: %{x}<br>%{yaxis.title.text}: %{y}<extra></extra>",
+                hovertemplate="<b>%{text}</b><br>%{yaxis.title.text}: %{y}<br>%{xaxis.title.text}: %{x}<extra></extra>",
                 name=attribute_name,
             )
             trace_list.append(designs_scatter)
@@ -317,7 +404,7 @@ class TradespaceDesigner:
                     symbol="square",
                 ),
                 text=[f"ID: {i}" for i in range(len(self.tradespace_df))],
-                hovertemplate="<b>%{text}</b><br><br>%{xaxis.title.text}: %{x}<br>%{yaxis.title.text}: %{y}<extra></extra>",
+                hovertemplate="<b>%{text}</b><br>%{yaxis.title.text}: %{y}<br>%{xaxis.title.text}: %{x}<extra></extra>",
                 name='All Designs',
             )
             trace_list.append(designs_scatter)
@@ -342,6 +429,9 @@ class TradespaceDesigner:
 
         pareto_fig = go.Figure(data=trace_list)
 
+        if desired_price_range:
+            pareto_fig = self.add_price_region(pareto_fig, desired_price_range)
+
         # Update the layout
         pareto_fig.update_layout(
             xaxis=dict(title=x_name), yaxis=dict(title=y_name), showlegend=showlegend
@@ -352,6 +442,34 @@ class TradespaceDesigner:
             pareto_fig.show()
 
         return designs_scatter, pareto_front_scatter
+
+    def plot_price_vs_mau(self, desired_price_range=[], block=True):
+
+        # generate scatter plot
+        designs_scatter, pareto_front_scatter = self.plot_tradespace_plotly('price', 'MAU', block=False)
+
+        # create a figure with the scatter plot
+        fig = go.Figure(data=[designs_scatter, pareto_front_scatter])
+
+        # change y axis limits to 0 to 1
+        fig.update_yaxes(range=[-0.05, 1])
+
+        if desired_price_range:
+            fig = self.add_price_region(fig, desired_price_range)
+
+        # update the layout
+        fig.update_layout(
+            xaxis=dict(title='Price (USD)'), yaxis=dict(title='MAU'), showlegend=True
+        )
+
+        # show the figure
+        if block:
+            fig.show()
+
+        return fig
+
+    # def plot_price_vs_performances(self, desired_price_range=desire_price_range):
+    #     pass
 
     def csv_plotly(self, block=True):
 
